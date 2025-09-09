ae73debf69eeb4fe435db31603e16d10675e8552
sc3p73r-it <sc3p73r@outlook.com>
2025-09-09 15:17:41 +0630

    Update output.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "aws_lab_tfstate_bucket_arn" {
-    value = data.aws_s3_bucket.aws_lab_tfstate.id
+    value = resource.aws_s3_bucket.aws_lab_tfstate.id
 }
 
 output "dynamodb_table_name" {
-    value = data.aws_dynamodb_table.terraform_locks.id
+    value = resource.aws_dynamodb_table.terraform_locks.id
 }
